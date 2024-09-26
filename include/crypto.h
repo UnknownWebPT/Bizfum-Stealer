4b00de0263377c202c7233a20abd076c6d0ea4e8
UnknownWebPT <exomsecowner@gmail.com>
2024-09-26 01:18:54 +0000

    Update crypto.h

--- include/crypto.h
+++ include/crypto.h
@@ -2,9 +2,12 @@
 #define CRYPTO_H
 
 #include <windows.h>
-#include <winhttp.h>
 
 int Base64DecodingFunc(const char *input, char **outputDec, DWORD *outputDecSize);
 NTSTATUS AES_256_GCM(const BYTE *encryptedData, ULONG encryptedDataLength, const BYTE *aad, ULONG aadLength, const BYTE *iv, ULONG ivLength, const BYTE *authTag, ULONG authTagLength, const BYTE *key, ULONG keyLength, BYTE *decryptedData, ULONG decryptedDataLength);
-
+typedef enum { siBuffer } SECItemType;
+typedef struct SECItemStr { SECItemType type; unsigned char *data; unsigned int len; } SECItem;
+typedef enum _SECStatus { SECWouldBlock = -2, SECFailure = -1, SECSuccess = 0, } SECStatus;
+typedef int (NSS_InitFunc)(const char *profilePath);
+typedef int (PK11SDR_DecryptFunc)(SECItem *data, SECItem *result, void *cx);
 #endif
